3db26542434b934d5c3353262203d81f55d1b3da
Feoridin <felumbaring@northislandcollege.ca>
2023-10-06 06:43:19 -0700

    ADD FIREBASE INITIALIZATION SCRIPT

--- smilechart.js
+++ smilechart.js
@@ -20,3 +20,16 @@ function w3_close() {
   mySidebar.style.display = "none";
   overlayBg.style.display = "none";
 }
+//Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyC4vkPVUjbqnGAoDGmXLFYm5WOuR4-PhyQ",
+    authDomain: "dgl-409.firebaseapp.com",
+    databaseURL: "https://dgl-409-default-rtdb.firebaseio.com",
+    projectId: "dgl-409",
+    storageBucket: "dgl-409.appspot.com",
+    messagingSenderId: "587821176763",
+    appId: "1:587821176763:web:e9e9a293cd59725320969e",
+    measurementId: "G-QSD6CP8DG3"
+    };
+
+    firebase.initializeApp(firebaseConfig);
